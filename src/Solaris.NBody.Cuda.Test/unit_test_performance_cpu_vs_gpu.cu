3af3c574ea5f18511451822f0421212246f27dca
Laszlo Dobos <dobos@complex.elte.hu>
2014-03-17 19:20:19 +0100

    Performance code removed to compile on linux

--- src/Solaris.NBody.Cuda.Test/unit_test_performance_cpu_vs_gpu.cu
+++ src/Solaris.NBody.Cuda.Test/unit_test_performance_cpu_vs_gpu.cu
@@ -53,35 +53,37 @@ static cudaError_t HandleError(cudaError_t cudaStatus, const char *file, int lin
 
 int64_t GetTimeMicro64()
 {
-#ifdef WIN32
-	/* Windows */
-	FILETIME ft;
-	LARGE_INTEGER li;
+//#ifdef WIN32
+//	/* Windows */
+//	FILETIME ft;
+//	LARGE_INTEGER li;
+//
+//	/* Get the amount of 100 nano seconds intervals elapsed since January 1, 1601 (UTC) and copy it
+//	* to a LARGE_INTEGER structure. */
+//	GetSystemTimeAsFileTime(&ft);
+//	li.LowPart = ft.dwLowDateTime;
+//	li.HighPart = ft.dwHighDateTime;
+//
+//	uint64_t ret = li.QuadPart;
+//	ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
+//	ret /= 10; /* From 100 nano seconds (10^-7) to 1 microsecond (10^-6) intervals */
+//
+//	return ret;
+//#else
+//	/* Linux */
+//	struct timeval tv;
+//
+//	gettimeofday(&tv, NULL);
+//
+//	uint64 ret = tv.tv_usec;
+//
+//	/* Adds the seconds (10^0) after converting them to microseconds (10^-6) */
+//	ret += (tv.tv_sec * 1000000);
+//
+//	return ret;
+//#endif
 
-	/* Get the amount of 100 nano seconds intervals elapsed since January 1, 1601 (UTC) and copy it
-	* to a LARGE_INTEGER structure. */
-	GetSystemTimeAsFileTime(&ft);
-	li.LowPart = ft.dwLowDateTime;
-	li.HighPart = ft.dwHighDateTime;
-
-	uint64_t ret = li.QuadPart;
-	ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
-	ret /= 10; /* From 100 nano seconds (10^-7) to 1 microsecond (10^-6) intervals */
-
-	return ret;
-#else
-	/* Linux */
-	struct timeval tv;
-
-	gettimeofday(&tv, NULL);
-
-	uint64 ret = tv.tv_usec;
-
-	/* Adds the seconds (10^0) after converting them to microseconds (10^-6) */
-	ret += (tv.tv_sec * 1000000);
-
-	return ret;
-#endif
+	return 0;
 }
 
 string create_number_of_bodies_str(const number_of_bodies *nBodies)
